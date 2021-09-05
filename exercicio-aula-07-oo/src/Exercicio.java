40f34fa92755c0b1793c4f56bea0ce9565de2d29
Joacy Farias <joacyfarias@yahoo.com.br>
2021-09-05 16:19:32 -0300

    Exercicio aula 07 - Relacionamento de agregação

--- exercicio-aula-07-oo/src/Exercicio.java
+++ exercicio-aula-07-oo/src/Exercicio.java
@@ -1,7 +1,7 @@
 public class Exercicio {
     public static void main(String[] args) {
-        Personagem p1 = new Personagem("Mario",100, 1.30f, 2, 30);
-        Personagem p2 = new Personagem("Luigi", 80, 1.50f, 3, 50);
+        Personagem p1 = new Personagem("Mario",100, 1.60f, 2, 30);
+        Personagem p2 = new Personagem("Luigi", 80, 1.80f, 3, 50);
 
         p1.estado();
         p2.estado();
